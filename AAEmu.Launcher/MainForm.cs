385ca39e9b19dfd42cd2d2b51b8f62879cd7ba93
ZeromusXYZ <zeromus.xyz@gmail.com>
2021-06-06 15:49:57 +0200

    Workflows

--- AAEmu.Launcher/MainForm.cs
+++ AAEmu.Launcher/MainForm.cs
@@ -843,11 +843,15 @@ namespace AAEmu.Launcher
                     btnLocaleLang.Image = Properties.Resources.mini_locale_cn;
                     break;
                 case settingsLangEN_US:
-                default:
                     Setting.Lang = settingsLangEN_US;
                     btnLocaleLang.Image = Properties.Resources.mini_locale_en_us;
                     break;
+                default:
+                    //Setting.Lang = settingsLangEN_US;
+                    btnLocaleLang.Image = Properties.Resources.mini_locale_unknown;
+                    break;
             }
+            ttLocale.SetToolTip(btnLocaleLang, Setting.Lang);
 
             btnLocaleLang.Refresh();
         }
@@ -2348,7 +2352,7 @@ namespace AAEmu.Launcher
                 {
                     ToolStripMenuItem mi = (ToolStripMenuItem)tsi;
                     {
-                        mi.Enabled = (mi.Tag.ToString() != Setting.Lang);
+                        mi.Enabled = (mi.Tag?.ToString() != Setting.Lang);
                     }
                 }
             }
@@ -3715,6 +3719,58 @@ namespace AAEmu.Launcher
         private void btnSystem_MouseLeave(object sender, EventArgs e)
         {
             btnSystem.Image = Properties.Resources.btn_aaemu;
+        }
+
+        public static DialogResult InputBox(string title, string promptText, ref string value)
+        {
+            Form form = new Form();
+            Label label = new Label();
+            TextBox textBox = new TextBox();
+            Button buttonOk = new Button();
+            Button buttonCancel = new Button();
+
+            form.Text = title;
+            label.Text = promptText;
+            textBox.Text = value;
+
+            buttonOk.Text = "OK";
+            buttonCancel.Text = "Cancel";
+            buttonOk.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+
+            label.SetBounds(9, 20, 372, 13);
+            textBox.SetBounds(12, 36, 372, 20);
+            buttonOk.SetBounds(228, 72, 75, 23);
+            buttonCancel.SetBounds(309, 72, 75, 23);
+
+            label.AutoSize = true;
+            textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+            buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+            form.ClientSize = new Size(396, 107);
+            form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+            form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.MinimizeBox = false;
+            form.MaximizeBox = false;
+            form.AcceptButton = buttonOk;
+            form.CancelButton = buttonCancel;
+
+            DialogResult dialogResult = form.ShowDialog();
+            value = textBox.Text;
+            return dialogResult;
+        }
+
+        private void unknownToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string v = Setting.Lang;
+            if (InputBox(L.UpdateLocale,"Custom Locale",ref v) == DialogResult.OK)
+            {
+                Setting.Lang = v;
+                UpdateLocaleLanguage();
+            }
         }
     }
 }
